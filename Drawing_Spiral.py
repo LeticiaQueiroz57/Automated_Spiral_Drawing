7ac0cd9f2ba0ba4d5ef93880bd64a9a640b52f5b
Letícia <leticia.queiroz5754@gmail.com>
2022-07-28 21:21:06 -0300

    Spiral

--- Drawing_Spiral.py
+++ Drawing_Spiral.py
@@ -7,12 +7,12 @@ bot.press("win")
 bot.write("paint")
 bot.press("enter")
 bot.moveTo(358, 169)
-while distance > 0:
-    bot.dragRel(distance, 0, 0.2, button="left")
+while distance > 20:
+    bot.dragRel(distance, 0, button="left")
     distance -= 20
-    bot.dragRel(0, distance, 0.2, button="left")
+    bot.dragRel(0, distance, button="left")
     distance -= 20
-    bot.dragRel(-distance, 0, 0.2, button="left")
+    bot.dragRel(-distance, 0, button="left")
     distance -= 20
-    bot.dragRel(0, -distance, 0.2, button="left")
+    bot.dragRel(0, -distance, button="left")
     distance -= 20
